2e594a99865953775a5ff0bee640f031b11a4a5d
chesterlsc <114641208+chesterlsc@users.noreply.github.com>
2022-11-04 18:56:13 +0800

    Add aos

--- assets/js/main.js
+++ assets/js/main.js
@@ -45,12 +45,12 @@ window.addEventListener('scroll', scrollActive)
 const sr = ScrollReveal({
     origin: 'top',
     distance: '60px',
-    duration: 2000,
+    duration: 2500,
     delay: 200,
 //     reset: true
 });
 
-sr.reveal('.home__data, .about__img, .skills__subtitle, .skills__text',{}); 
+sr.reveal('.home__data, .about__img, .skills__subtitle, .skills__text, .recent',{}); 
 sr.reveal('.home__img, .about__subtitle, .about__text, .skills__img',{delay: 400}); 
 sr.reveal('.home__social-icon',{ interval: 200}); 
 sr.reveal('.skills__data, .work__img, .contact__input',{interval: 200}); 
